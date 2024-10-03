923614236f7d5e21b30354fa7f4bd5a9765cc9b2
Jesse Maarleveld <j.maarleveld@rug.nl>
2024-10-03 17:13:32 +0200

    Safe work

---
+++ replication/compute_network_metrics.py
@@ -0,0 +1,216 @@
+import json
+import math
+import os
+import re
+import xml.etree.ElementTree
+
+import alive_progress
+import networkx
+import numpy
+import polars
+from alive_progress import alive_bar
+
+DATA_DIRECTORY = 'data'
+
+
+def main():
+    if not os.path.exists(DATA_DIRECTORY):
+        raise ValueError('Data directory does not exist. Be sure to run `prepare_data.py` first!')
+    for filename in os.listdir(DATA_DIRECTORY):
+        path = os.path.join(DATA_DIRECTORY, filename)
+        if not os.path.isdir(path):
+            continue
+        for file in os.listdir(path):
+            full_path = os.path.join(path, file)
+            if not file.endswith('.odem'):
+                continue
+            metrics = generate_metrics(full_path)
+            with open(full_path.replace('.odem', '.json'), 'w') as f:
+                json.dump(metrics, f, indent=2)
+
+
+def generate_metrics(filename):
+    df = polars.read_csv(_find_text_features(filename))
+    graph = Graph.from_xml(filename)
+    data = {
+        'nodes': list(graph.nodes),
+        'edges': [
+            {'from': key[0], 'to': key[1]}
+            for key in graph.edges
+        ],
+        'network-features': []
+    }
+    n = len(graph.nodes)
+    total = n**2 - n
+    with alive_progress.alive_bar(total) as progress:
+        for x in graph.nodes:
+            #if graph.nodes[x] is None:
+            #    continue
+            for y in graph.nodes:
+                #if graph.nodes[x] is None:
+                #    continue
+                if x == y:
+                    continue
+                entry = {
+                    'from': x,
+                    'to': y,
+                    'features': {
+                        'common_neighbours': graph.common_neighbours(x, y),
+                        'salton': graph.salton(x, y),
+                        'sorensen': graph.sorensen(x, y),
+                        'adamic_adar': graph.adamic_adar(x, y),
+                        'katz': graph.katz(x, y),
+                        'sim_rank': graph.sim_rank(x, y),
+                        'russel_rao': graph.russel_rao(x, y),
+                        'resource_allocation': graph.resource_allocation(x, y),
+                    }
+                }
+                data['network-features'].append(entry)
+                progress()
+    return data
+
+
+class Graph:
+
+    def __init__(self, spec):
+        self.nodes = set()
+        self.edges = set()
+        # for (node, node_type), edges in spec.items():
+        #     self.nodes[node] = node_type
+        #     for (target, edge_type) in edges:
+        #         if target not in self.nodes:
+        #             self.nodes[target] = None
+        #         self.edges[(node, target)] = edge_type
+        # no_types = []
+        # for node, node_type in self.nodes.items():
+        #     if node_type is None and not node.startswith('java.'):
+        #         no_types.append(node)
+        # if no_types:
+        #     # raise ValueError(f'The following nodes have no type: {no_types}')
+        #     pass
+        for node, edges in spec.items():
+            self.nodes.add(node)
+            for edge in edges:
+                self.nodes.add(edge)
+                self.edges.add((node, edge))
+        self.graph = networkx.Graph()
+        self.graph.add_nodes_from(self.nodes)
+        self.graph.add_edges_from(self.edges)
+        self._katz = None
+
+
+    @classmethod
+    def from_xml(cls, filename):
+        tree = xml.etree.ElementTree.parse(filename)
+        root = tree.getroot()
+        context = cls._find_one_tag(root, 'context')
+        container = cls._find_one_tag(context, 'container')
+        nodes = {}
+        for namespace in container:
+            assert namespace.tag == 'namespace'
+            for child in namespace:
+                assert child.tag == 'type'
+                source = child.get('name')
+                source_type = child.get('classification')
+                assert source is not None
+                assert source_type is not None
+                dependencies = cls._find_one_tag(child, 'dependencies')
+                for dep in dependencies:
+                    assert dep.tag == 'depends-on'
+                    target = dep.get('name')
+                    edge_type = dep.get('classification')
+                    assert target is not None
+                    assert edge_type is not None
+                    nodes.setdefault((source, source_type), []).append((target, edge_type))
+        return cls(cls._lift_to_packages(nodes))
+
+    @staticmethod
+    def _find_one_tag(tag, name):
+        child = None
+        for c in tag:
+            if c.tag == name:
+                if child is not None:
+                    raise ValueError(f'Duplicate child {name}')
+                child = c
+        return child
+
+    @staticmethod
+    def _lift_to_packages(cls_diagram):
+        package_diagram = {}
+        for (node, _), edges in cls_diagram.items():
+            src_cls = '.'.join(node.split('.')[:-1])
+            for to, _ in edges:
+                to_cls = '.'.join(to.split('.')[:-1])
+                package_diagram.setdefault(src_cls, set()).add(to_cls)
+        return package_diagram
+
+    def common_neighbours(self, node_a, node_b):
+        return len(set(networkx.common_neighbors(self.graph, node_a, node_b)))
+
+    def salton(self, node_a, node_b):
+        # https://www-sciencedirect-com.proxy-ub.rug.nl/science/article/pii/S037843711000991X
+        x = self.graph.neighbors(node_a)
+        y = self.graph.neighbors(node_b)
+        z = set(x) & set(y)
+        dx = self.graph.degree(node_a)
+        dy = self.graph.degree(node_b)
+        dz = math.sqrt(dx * dy)         # type: ignore
+        return len(z) / dz
+
+    def sorensen(self, node_a, node_b):
+        # https://www-sciencedirect-com.proxy-ub.rug.nl/science/article/pii/S037843711000991X
+        x = self.graph.neighbors(node_a)
+        y = self.graph.neighbors(node_b)
+        z = set(x) & set(y)
+        dx = self.graph.degree(node_a)
+        dy = self.graph.degree(node_b)
+        return 2 * len(z) / (dx + dy)   # type: ignore
+
+    def adamic_adar(self, node_a, node_b):
+        items = list(networkx.adamic_adar_index(self.graph, [(node_a, node_b)]))
+        return items[0][-1]
+
+    def katz(self, node_a, node_b):
+        # Based on
+        # https://evalne.readthedocs.io/en/latest/_modules/evalne/methods/katz.html#Katz
+        if self._katz is None:
+            adj = networkx.adjacency_matrix(self.graph)
+            beta = 0.005
+            aux = adj.T.multiply(-beta).todense()
+            numpy.fill_diagonal(aux, 1 + aux.diagonal())
+            sim = numpy.linalg.inv(aux)
+            numpy.fill_diagonal(sim, sim.diagonal() - 1)
+            self._katz = sim
+        nodes = list(self.graph.nodes())
+        i = nodes.index(node_a)
+        j = nodes.index(node_b)
+        assert abs(self._katz[i, j] - self._katz[j, i]) <= 1e-4
+        return self._katz[i, j]
+
+    def sim_rank(self, node_a, node_b):
+        return networkx.simrank_similarity(self.graph, source=node_a, target=node_b)
+
+    def russel_rao(self, node_a, node_b):
+        # based on decompiled version of the code of Tomassel et al.
+        x = self.graph.neighbors(node_a)
+        y = self.graph.neighbors(node_b)
+        z = set(x) & set(y)
+        return len(z) / len(self.graph.nodes)
+
+    def resource_allocation(self, node_a, node_b):
+        items = list(networkx.resource_allocation_index(self.graph, [(node_a, node_b)]))
+        return items[0][-1]
+
+
+def _find_text_features(path):
+    prefix = re.match(r'[a-zA-Z_\-0-9]+-\d+(\.\d+)*', path).group()
+    directory, _ = os.path.split(path)
+    for filename in os.listdir(directory):
+        if filename.startswith(prefix):
+            return os.path.join(directory, filename)
+    raise ValueError(f'Could not find semantic features for file {path}')
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ replication/prepare_data.py
@@ -0,0 +1,55 @@
+import os.path
+import urllib.request
+import zipfile
+
+REPO_LINK = 'https://github.com/tommantonela/ASPredictor/raw/refs/heads/master/reproducibility-kit'
+ARCHIVES = (
+    'apache-ant.zip',
+    'apache-derby.zip'
+)
+DATA_DIRECTORY = 'data'
+MANUAL = {
+    'apache-camel.zip': 'https://drive.google.com/file/d/1qyCflQ9DFoSpUcc6TotW-8L8uNV7djr2/view?usp=sharing',
+    'apache-cxf.zip': 'https://drive.google.com/file/d/1sJ1F_RuAVSa-iQbWQLHEQkxtNYpoZKJF/view?usp=sharing',
+    'hibernate.zip': 'https://drive.google.com/file/d/1yxyUod2XjjK5fADJDNFZo9NjAIJiTDaI/view?usp=sharing'
+}
+ALL_ARCHIVES = ARCHIVES + tuple(MANUAL.keys())
+
+
+def download():
+    os.makedirs(DATA_DIRECTORY, exist_ok=True)
+    for archive in ARCHIVES:
+        full_path = os.path.join(DATA_DIRECTORY, archive)
+        if not os.path.exists(full_path):
+            url = f'{REPO_LINK}/{archive}'
+            print(f'Downloading {archive} from {url}...')
+            urllib.request.urlretrieve(url, full_path)
+    print('Done!')
+    manual = []
+    for archive, url in MANUAL.items():
+        if not os.path.exists(os.path.join(DATA_DIRECTORY, archive)):
+            manual.append((archive, url))
+    if manual:
+        print('Please download the following archives manually:')
+        for i, (archive, url) in enumerate(manual, start=1):
+            print(f'{i}. {archive} -- {url}')
+    return not bool(manual)
+
+
+def unpack():
+    for archive in ALL_ARCHIVES:
+        source = os.path.join(DATA_DIRECTORY, archive)
+        print(f'Unpacking {source}...')
+        with zipfile.ZipFile(source, 'r') as zf:
+            zf.extractall(DATA_DIRECTORY)
+    print('Done!')
+
+
+def main():
+    if not download():
+        return
+    unpack()
+
+
+if __name__ == '__main__':
+    main()
