77da0be583dd79b90c2751597104e282c2fd5249
Jesse Maarleveld <j.maarleveld@rug.nl>
2024-10-29 16:42:12 +0100

    Module interface change

--- pipeline/src/graphs/loaders.rs
+++ pipeline/src/graphs/loaders.rs
@@ -1,2 +1,28 @@
-pub(crate) mod odem;
-pub(crate) mod rsf;
+use std::path::Path;
+use crate::graphs::DependencyGraph;
+use crate::graphs::loaders::odem::OdemGraphRoot;
+use crate::graphs::loaders::rsf::FromRsfFile;
+
+mod odem;
+mod rsf;
+
+
+pub fn load_odem_graph(path: impl AsRef<Path>) -> anyhow::Result<DependencyGraph> {
+    let odem = OdemGraphRoot::load_from_file(path)?;
+    Ok(DependencyGraph::from(odem))
+}
+
+pub fn load_rsf_graph(path: impl AsRef<Path>) -> anyhow::Result<DependencyGraph> {
+    let g =  DependencyGraph::load_from_rsf_file(path)?;
+    Ok(g)
+}
+
+pub fn load_graph_from_file(path: impl AsRef<Path>) -> anyhow::Result<DependencyGraph> {
+    let ext = path.as_ref().extension()
+        .ok_or_else(|| anyhow::anyhow!("Need a file extension"))?;
+    match ext.to_str().expect("Failed to convert file extension to string") {
+        "rsf" => load_rsf_graph(path),
+        "odem" => load_odem_graph(path),
+        x => Err(anyhow::anyhow!("Unknown file extension: {}", x))
+    }
+}
